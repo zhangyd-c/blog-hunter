1d60eaba8753e02c5215e87fda910cebaa5820e5
yadong.zhang <yadong.zhang0415@gmail.com>
2019-03-05 18:46:36 +0800

    :alien: 由于外部API的改变而更新代码

--- src/main/java/me/zhyd/hunter/config/HunterDateDeserializer.java
+++ src/main/java/me/zhyd/hunter/config/HunterDateDeserializer.java
@@ -1,7 +1,8 @@
 package me.zhyd.hunter.config;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.parser.DefaultJSONParser;
-import com.alibaba.fastjson.parser.deserializer.DateDeserializer;
+import com.alibaba.fastjson.parser.deserializer.ObjectDeserializer;
 import me.zhyd.hunter.util.DateUtil;
 
 import java.lang.reflect.Type;
@@ -15,7 +16,7 @@ import java.lang.reflect.Type;
  * @date 2019/2/26 19:14
  * @since 1.8
  */
-public class HunterDateDeserializer extends DateDeserializer {
+public class HunterDateDeserializer implements ObjectDeserializer {
     public static final HunterDateDeserializer instance = new HunterDateDeserializer();
 
     public HunterDateDeserializer() {
@@ -23,5 +24,19 @@ public class HunterDateDeserializer extends DateDeserializer {
 
     protected <T> T cast(DefaultJSONParser parser, Type clazz, Object fieldName, Object val) {
         return (T) DateUtil.parse(val);
+    }
+
+    @Override
+    public <T> T deserialze(DefaultJSONParser defaultJSONParser, Type type, Object o) {
+        JSONObject object = JSONObject.parseObject(defaultJSONParser.getInput());
+        if (null != o && o.equals("releaseDate")) {
+            return (T) DateUtil.parse(object.get(o));
+        }
+        return (T) object.get(o);
+    }
+
+    @Override
+    public int getFastMatchToken() {
+        return 0;
     }
 }
